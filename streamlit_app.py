54aec57618f08078a1684c92451d5286b4f7c4ed
athulcp093 <83904435+athulcp093@users.noreply.github.com>
2023-09-26 17:51:00 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("Hii guys")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale,Spinach & Rocket Smoothiee")
+streamlit.text("Hard-Boiled Free-Range egg")
