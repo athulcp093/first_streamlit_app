18f63425bae7b81c305b0424b70948a52e0b1a04
athulcp093 <83904435+athulcp093@users.noreply.github.com>
2023-09-27 10:39:07 +0530

    Update streamlit_app.py
    added breakfast menu and 4 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title("Hii guys")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale,Spinach & Rocket Smoothiee")
-streamlit.text("Hard-Boiled Free-Range egg")
+streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale,Spinach & Rocket Smoothiee")
+streamlit.text("🐔Hard-Boiled Free-Range egg")
+streamlit.text("🥑🍞Avocado Toast")
+   
